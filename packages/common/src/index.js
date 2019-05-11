cde589a362058320c6c768a2bfe9690605f6bd5b
GZtheStackJourney <GZliew>
2019-05-11 23:02:44 +0800

    Initial files

---
+++ packages/common/src/index.js
@@ -0,0 +1,4 @@
+"use strict";
+exports.__esModule = true;
+exports.add = function (a, b) { return a + b; };
+//# sourceMappingURL=index.js.map
---
+++ packages/server/src/index.ts
@@ -0,0 +1,3 @@
+import { add } from "@peanut/common";
+
+console.log(add(1, 2));
